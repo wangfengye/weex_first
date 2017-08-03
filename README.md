850a507391b7cb5110df505254efd09faa5bc809
feng <https://git.oschina.net/fengwangfengye/Wi-Fi-Log.git>
2017-08-03 09:49:58 +0800

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+
+#### 操作指令
+1 全局安装 weexpack
+    npm install weexpack -g
+2 生成项目
+    weexpack create name
+3 进入项目根目录 安装依赖
+    npm install
+4 安装应用模板,生成android后ios工程
+    weexpack platform add android 
+    weexpack platform add ios
+5 打包apk
+    weexpack run android
+6 web打包
+    weexpack build web
+7 web运行
+    weexpack run web
+8 热更新
+    npm run web_watch
+    
---
+++ src/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="wrapper">
+    <text class="title">ddwdd</text>
+    <text class="desc">Now, update</text>
+  </div>
+</template>
+
+<style>
+  .wrapper { align-items: center; margin-top: 120px; }
+  .title { padding-top:40px; padding-bottom: 40px; font-size: 48px; }
+  .desc { padding-top: 20px; color:#888; font-size: 24px;}
+</style>
+
+<script>
+  export default {
+   
+  }
+</script>
